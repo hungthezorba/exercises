ecd7061c1286b6a76cdb23347da1767e5f6ed87e
hungnguyen <hungthezorba@gmail.com>
2020-11-01 10:40:29 +0700

    Update solution

--- C++/5-1.cpp
+++ C++/5-1.cpp
@@ -3,29 +3,39 @@
 using namespace std;
 
 bool isPrime(int number) {
-	if (number > 1 && number < 1000) {
-		int count = 0;
-		for (int i = 2; i <= number / 2; i++) {
-			if (number % i == 0)
-				count++;
+	bool check = true;
+	for (int i = 2; i <= number / 2; i++) {
+		if (number % i == 0){
+			check = false;
+			return check;
 		}
-		cout << count << endl;
-		if (count > 0) {
-			return false;
-		} else {
-			return true;
-		}
-	} else {
-		return false;
 	}
+	return check;
+} 
+
+bool isInRange(int number) {
+	if (number > 1 && number < 1000)
+		return true;
+	else
+		return false;
 }
 
 int main() {
-	//
-	if (isPrime(4)) {
-		cout << "True" << endl;
+	// Initialize
+	int number;
+
+	cout << "Input an integer number between 1 - 1000: ";
+	cin >> number;
+
+	if (isInRange(number)) {
+		if (isPrime(number)) {
+			cout << "True" << endl;
+		} else {
+			cout << "False" << endl;
+		}
 	} else {
-		cout << "False" << endl;
+		cout << "Number not in range 1 - 1000" << endl;
 	}
+
 	return 0;
 }
